aef3bd4f7a4dce9431a0344d3a0f07021a2bc66c
Jesús García <ctw@ctwhome.com>
2024-05-06 11:52:15 +0200

    chore: Update fallback page to 404.html in svelte.config.js

--- dashboard/svelte.config.js
+++ dashboard/svelte.config.js
@@ -12,7 +12,7 @@ const config = {
 		// If your environment is not supported or you settled on a specific environment, switch out the adapter.
 		// See https://kitp dev.svelte.dev/docs/adapters for more information about adapters.
 		adapter: adapter({
-			fallback: '200.html' // may differ from host to host
+			fallback: '404.html' // may differ from host to host
 		}),
 		paths: {
 			base: process.argv.includes('dev') ? '' : process.env.BASE_PATH
